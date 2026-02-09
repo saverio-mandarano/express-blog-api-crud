cb051aeb245c0ff60c7d216565fda676db9878fd
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-09 19:18:30 +0100

    Add data/posts.j, public/images and activation public folder

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//attivazione della cartella public per uso file statici
+app.use(express.static("public"));
+
+// rotta home APP
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
